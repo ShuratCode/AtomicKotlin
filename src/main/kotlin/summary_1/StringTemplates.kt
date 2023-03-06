5ae042673fcf603c9ccdbfedacfe4a551402c97c
Shaked Eyal <shaked.eyal@tufin.com>
2023-03-06 23:20:08 +0200

    Add String Templates examples

---
+++ src/main/kotlin/summary_1/StringTemplates.kt
@@ -0,0 +1,15 @@
+package summary_1
+
+fun main() {
+    val answer = 42
+    println("Found $answer!")
+    val condition = true
+    println("${if (condition) 'a' else 'b'}")
+    println("printing a $1")
+}
+
+/* Output:
+Found 42!
+a
+printing a $1
+*/
