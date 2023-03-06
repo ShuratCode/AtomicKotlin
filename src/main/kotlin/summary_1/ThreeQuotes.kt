25d95641511642c2ec83ed0e644aac03505428f4
Shaked Eyal <shaked.eyal@tufin.com>
2023-03-06 23:38:07 +0200

    Add example for multiline String Templates

---
+++ src/main/kotlin/summary_1/ThreeQuotes.kt
@@ -0,0 +1,18 @@
+package summary_1
+
+fun json(q: String, a: Int) = """{
+    "question: "$q",
+     "answer": $a
+}""".trimIndent()
+
+fun main() {
+    val jsonResult = json("The Ultimate", 42)
+    println(jsonResult)
+}
+
+/* Output:
+{
+    "question: "The Ultimate",
+    "answer": 42
+}
+*/
